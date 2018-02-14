085ac1af8cb95dcc0f3bc14da29701c7b657bc9b
GeorgiNzm <georginzm@gmail.com>
2018-02-14 11:45:53 +0200

    Developing Android App - create enum with URLs

---
+++ RemoteCarControllerClient/app/src/main/java/elsys/org/remotecarcontrollerclient/MainActivity.java
@@ -0,0 +1,37 @@
+package elsys.org.remotecarcontrollerclient;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enableFullScreenLandscape();
+    }
+
+    private void enableFullScreenLandscape() {
+        //Enable fullscreen and change window orientation
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+}
