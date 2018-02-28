0cfddd90679d012612ccd3e1bbd7a8171d1ab60b
GeorgiNzm <georginzm@gmail.com>
2018-02-28 16:13:56 +0200

    Initializing buttons and setting listeners in MainActivity

--- RemoteCarControllerClient/app/src/main/java/org/elsys/remote_control_car/MainActivity.java
+++ RemoteCarControllerClient/app/src/main/java/org/elsys/remote_control_car/MainActivity.java
@@ -3,14 +3,32 @@ package org.elsys.remote_control_car;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.WindowManager;
+import android.widget.ImageButton;
+
+import org.elsys.remote_control_car.implementation.OnTouchListenerImpl;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Map<String, ImageButton> buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeButtons();
+
+        try {
+            setListeners();
+        } catch (NullPointerException nex) {
+            Log.d("NULL-P", nex.getMessage());
+        }
     }
 
     private void enableFullScreenLandscape() {
@@ -18,5 +36,29 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                              WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
+
+    private void initializeButtons() {
+        buttons = new HashMap<String, ImageButton>();
+
+        buttons.put("Forward", (ImageButton) findViewById(R.id.forward_btn));
+        buttons.put("Backward", (ImageButton) findViewById(R.id.backward_btn));
+        buttons.put("Right", (ImageButton) findViewById(R.id.right_btn));
+        buttons.put("Left", (ImageButton) findViewById(R.id.left_btn));
+    }
+
+    private void setListeners() {
+        Iterator<String> iterator = buttons.keySet().iterator();
+
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            ImageButton button = buttons.get(key);
+
+            if ((key == null) || (button == null)) {
+                throw new NullPointerException("Setting button listener error");
+            }
+
+            button.setOnTouchListener(new OnTouchListenerImpl());
+        }
     }
 }
