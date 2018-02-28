0e339f9f7bb29f51ce41d9c96968ba5d6e4f873e
GeorgiNzm <georginzm@gmail.com>
2018-02-28 14:10:12 +0200

    Fixing sigleton pattern usage, renaming company domain

---
+++ RemoteCarControllerClient/app/src/main/java/org/elsys/remote_control_car/enums/RequestType.java
@@ -0,0 +1,14 @@
+package org.elsys.remote_control_car.enums;
+
+/**
+ * Created by georgi on 18.02.18.
+ */
+
+public enum RequestType {
+    MOVE_FORWARD,
+    MOVE_BACKWARD,
+    STEER_RIGHT,
+    STEER_LEFT,
+    STOP_MOVEMENT,
+    STOP_STEERING
+}
